ec2cb1eeb1e489dd025c11335cc0c16766c3571d
oma <oma@pbprog.ru>
2024-04-27 00:24:24 +0300

    Великая чистка

--- repos/ResultsRepository.py
+++ repos/ResultsRepository.py
@@ -2,10 +2,11 @@ from models.ResultsModel import ResultsModel, UserModel
 from repos.RaitingRepository import *
 
 medals = {
-    1:"🥇 1 место:",
-    2:"🥈 2 место:",
-    3:"🥉 3 место:"
+    1: "🥇 1 место:",
+    2: "🥈 2 место:",
+    3: "🥉 3 место:"
 }
+
 
 def save_results(user: UserModel, max_speed, distance):
     try:
@@ -23,23 +24,26 @@ def save_results(user: UserModel, max_speed, distance):
     except Exception as e:
         return None
 
+
 def get_all_results():
     message = "Результаты:\n\n"
     message += "Скорость:\n"
     raitings = get_all_raitings_by_speed()
-    for place,raiting in enumerate(raitings):
-        message+=f"{medals[place+1]} {raiting.user_id.user_name}: {round(raiting.max_speed, 2)} км\ч\n"
-    message+="\n\nДистанция:\n"
-    for place,raiting in enumerate(raitings):
+    for place, raiting in enumerate(raitings):
+        message += f"{medals[place + 1]} {raiting.user_id.user_name}: {round(raiting.max_speed, 2)} км\ч\n"
+    message += "\n\nДистанция:\n"
+    for place, raiting in enumerate(raitings):
         message += f"{medals[place + 1]} {raiting.user_id.user_name}: {round(raiting.distance, 2)} км\n"
     return message
-def get_results_for_user(user:UserModel):
+
+
+def get_results_for_user(user: UserModel):
     message = "Результаты:\n\n"
     message += "Скорость:\n"
     raitings = get_all_raitings_by_speed()
     for place, raiting in enumerate(raitings):
         if raiting.user_id.user_id == user.user_id:
-            message+=f"{medals[place+1]} {raiting.user_id.user_name}: {round(raiting.max_speed, 2)} км\ч\n"
+            message += f"{medals[place + 1]} {raiting.user_id.user_name}: {round(raiting.max_speed, 2)} км\ч\n"
     message += "\n\nДистанция:\n"
     for place, raiting in enumerate(raitings):
         if raiting.user_id.user_id == user.user_id:
