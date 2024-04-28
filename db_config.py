33e0bbf6d4c383c1e7c1dea7c6ce362d49e53131
oma <oma@pbprog.ru>
2024-04-29 00:54:37 +0300

    адаптация для докера

--- db_config.py
+++ db_config.py
@@ -1,12 +1,13 @@
 from peewee import PostgresqlDatabase
+import os
 # region Для докера
-# dbUsername = os.environ['POSTGRES_USER']
-# dbPassword = os.environ['POSTGRES_PASSWORD']
-# dbName = os.environ['POSTGRES_DB']
-# host = 'postgres'
+dbUsername = os.environ['POSTGRES_USER']
+dbPassword = os.environ['POSTGRES_PASSWORD']
+dbName = os.environ['POSTGRES_DB']
+host = 'postgres'
 # endregion
-dbUsername = "postgres"
-dbPassword = "1"
-dbName = "postgres"
-host = "localhost"
+# dbUsername = "postgres"
+# dbPassword = "1"
+# dbName = "postgres"
+# host = "localhost"
 db = PostgresqlDatabase(dbName, host=host, port=5432, user=dbUsername, password=dbPassword)
